35fdd9ebcc510711df0a1d106db79165fd52f16f
Melkov Denis <d.a.melkov@gmail.com>
2020-07-14 15:35:35 +0300

    +pressure fields

--- app/src/main/java/ru/startandroid/develop/melkovhw3/PressureActivity.java
+++ app/src/main/java/ru/startandroid/develop/melkovhw3/PressureActivity.java
@@ -3,6 +3,9 @@ package ru.startandroid.develop.melkovhw3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class PressureActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class PressureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pressure);
+
+        EditText edtSystolic = findViewById(R.id.edtSystolic);
+        EditText edtDiastolic = findViewById(R.id.edtDiastolic);
+        EditText edtPulse = findViewById(R.id.edtPulse);
+        Switch swTachycardia = findViewById(R.id.swTachycardia);
+
     }
 }
